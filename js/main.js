8173fed26a9399b3f4a465f0f9b3e1a1e9d59715
jussimar <jussimar@jussimarleal.com.br>
2025-02-21 22:28:50 -0300

    inserindo elementos de formulario

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 "use strict";
 var empresa;
 (function (empresa) {
-    let p = new empresa.Pessoa();
-    p.nome = "Augusto";
-    p.anoNasc = 1975;
-    document.getElementById("nome").textContent = p.nome;
-    document.getElementById("ano").textContent = p.anoNasc.toString();
-    document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    const calc = document.getElementById("calc");
+    const campoNome = document.getElementById("campoNome");
+    const campoAno = document.getElementById("campoAno");
+    calc.addEventListener("click", () => {
+        let p = new empresa.Pessoa();
+        p.nome = campoNome.value;
+        p.anoNasc = parseInt(campoAno.value);
+        document.getElementById("nome").textContent = p.nome;
+        document.getElementById("ano").textContent = p.anoNasc.toString();
+        document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    });
 })(empresa || (empresa = {}));
--- ts/main.ts
+++ ts/main.ts
@@ -1,9 +1,18 @@
 namespace empresa{
-    let p = new Pessoa();
-    p.nome = "Augusto";
-    p.anoNasc = 1975;
 
-    document.getElementById("nome").textContent = p.nome;
-    document.getElementById("ano").textContent = p.anoNasc.toString();
-    document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    const calc = document.getElementById("calc") as HTMLButtonElement;
+    const campoNome = document.getElementById("campoNome") as HTMLInputElement;
+    const campoAno = document.getElementById("campoAno") as HTMLInputElement;
+    
+    calc.addEventListener("click", ()=>{
+        let p = new Pessoa();
+        p.nome = campoNome.value;
+        p.anoNasc = parseInt(campoAno.value);
+
+        document.getElementById("nome").textContent = p.nome;
+        document.getElementById("ano").textContent = p.anoNasc.toString();
+        document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+    });
+
+    
 }
